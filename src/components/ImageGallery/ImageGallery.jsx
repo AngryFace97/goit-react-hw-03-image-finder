634e62f85602fa5aa488e93a8f0cd2b12a795226
Paweł Kłodowski <angryface97@gmail.com>
2022-08-27 15:32:22 +0200

    Create ImageGallery.jsx

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './ImageGallery.module.css';
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+import { nanoid } from 'nanoid';
+
+export const ImageGallery = ({items}) => {
+  const {gallery} = styles;
+
+  return (
+    <ul className={gallery}>
+      {items.map(({webformatURL, tags}) => <ImageGalleryItem key={nanoid()} src={webformatURL} alt={tags} />)}
+    </ul>
+  )
+}
+
+export default ImageGallery;
