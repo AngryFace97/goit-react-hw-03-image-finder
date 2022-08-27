dce076a920d5d036676037f14c1887c125720bc0
Paweł Kłodowski <angryface97@gmail.com>
2022-08-27 15:32:28 +0200

    Create Button.jsx

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+export const Button = ({loadMore}) => {
+  const {btn} = styles;
+
+  return (
+    <>
+     <button 
+      type="button" 
+      className={btn} 
+      onClick={loadMore}
+    >
+      Load more
+    </button> 
+    </>
+  )
+}
