460f5283299640570f15c7ebce2856992dbb2d06
Paweł Kłodowski <angryface97@gmail.com>
2022-08-27 15:32:32 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import * as api from './fetchImages';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+
+export class App extends Component {
+  state = {
+    images: [],
+    searchQuery: '',
+    page: 1,
+    isLoading: false,
+    error: null,
+    foundImages: null,
+  }
+
+  setInitialParams = (searchQuery) => {
+    if (searchQuery === '') {
+      return alert('Enter the search value!')
+    }
+
+    if (searchQuery === this.state.searchQuery) {
+      return;
+    }
+
+    this.setState({
+      images: [],
+      searchQuery,
+      page: 1,
+    });
+  }
+
+  loadMore = () => {
+    this.setState(({page}) => ({page: page + 1}));
+  }
+
+  addImages = async (searchQuery, page) => {
+    this.setState({ isLoading: true });
+
+    try {
+      const data = await api.fetchImagesWithQuery(searchQuery, page);
+      const {hits: newImages, totalHits: foundImages} = data;
+
+      this.setState(oldState => ({
+        images: [...oldState.images, ...newImages],
+      }));
+
+      if (foundImages !== this.state.foundImages) {
+        this.setState({ foundImages });
+      }
+    } catch (error) {
+      this.setState({ error })
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.page !== this.state.page || prevState.searchQuery !== this.state.searchQuery) {
+      const {searchQuery, page} = this.state;
+      this.addImages(searchQuery, page);
+    }
+  }
+
+  render() {
+    const {images, isLoading, error, foundImages} = this.state;
+
+    return (
+      <div>
+        <Searchbar onSubmit={this.setInitialParams}/>
+        {error && <p>Whoops, something went wrong: {error.message}</p>}
+        {isLoading && <Loader />}
+        {images.length > 0 && <ImageGallery items={images} />}
+        {images.length < foundImages && <Button loadMore={this.loadMore} />}
+      </div>
+    );
+  }
 };
+
+export default App;
