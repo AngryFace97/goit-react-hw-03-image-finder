58ef75e6a7c7312ad23c06b9d7530a8ba240d48c
Paweł Kłodowski <angryface97@gmail.com>
2022-08-28 13:18:04 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import styles from './ImageGallery.module.css';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
 
-export const ImageGallery = ({items}) => {
+export const ImageGallery = ({items, openModal}) => {
   const {gallery} = styles;
 
   return (
     <ul className={gallery}>
-      {items.map(({webformatURL, tags}) => <ImageGalleryItem key={nanoid()} src={webformatURL} alt={tags} />)}
+      {items.map(item => <ImageGalleryItem key={nanoid()} itemData={item} openModal={openModal}/>)}
     </ul>
   )
+}
+
+ImageGallery.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  openModal: PropTypes.func.isRequired,
 }
 
 export default ImageGallery;
